047d4f3020a75fa0758dd3fb99e05d5999afab97
ltraugot <ltraugot@risd.edu>
2017-01-24 08:17:33 -0500

    Panoptics

---
+++ exercises/project3/panoptics.js
@@ -0,0 +1,17 @@
+$(function(){
+
+// $("img").hover(function(){
+// 	$(".bluebox").slideToggle();
+// });
+
+// $(".caption").click(function(){
+//     $(".url").slideToggle();
+// });
+
+
+$("img").click(function(){
+    $(".title").slideToggle();
+    $(".caption").slideToggle();
+    $(".url").slideToggle();
+});
+});
