a70afcce0602eb18c24c0d1a531c1d5627823f41
ltraugot <ltraugot@risd.edu>
2017-02-07 01:36:55 -0500

    final update

---
+++ exercises/exercise13/map.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){ 
+
+$(".button").click(function(){
+    $(".text").slideToggle("slow");
+
+    // $(".button").click(function(){
+    // $(".text").hide();
+
+
+    //  $('#button').click(function() {
+    //     var $text = $('.text');
+    //     if ($text.is(':visible')) {
+    //         // Slide away
+    //         $text.animate({left: -($text.outerWidth() + 10)}, function() {
+    //             $text.hide();
+    //         });
+    //     }
+    //     else {
+    //         // Slide in
+    //         $text.show().animate({left: 0});
+    //     }
+    // });
+});
+
+});
